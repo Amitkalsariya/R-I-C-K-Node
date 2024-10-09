bd54dcf6a95e0dc5c63de86fa2c73d82e56986be
AMIT <156776217+Amitkalsariya@users.noreply.github.com>
2024-10-09 17:56:17 +0530

    Done

--- Project 1/app.js
+++ Project 1/app.js
@@ -2,8 +2,19 @@ const express=require('express')
 const fs =require('fs')
 const app=express()
 let list=[]
+
+const data=fs.readFileSync('data.json','utf-8')
+if(data != '')
+{
+    list=JSON.parse(data)
+}
 app.set('view engine','ejs')
 app.get('/',(req,res)=>{
-    res.render('index',{list})
+    res.render('index',{list,showData:null})
+})
+app.get('/showData',(req,res)=>{
+    const eId=req.query.show
+    let showData=list[eId]
+    res.render('product',{showData,list})
 })
 app.listen(1910)
---
+++ Project 2/app.js
@@ -0,0 +1,23 @@
+const express=require('express')
+const fs =require('fs')
+const app=express()
+let list=[]
+
+const data=fs.readFileSync('data.json','utf-8')
+if(data != '')
+{
+    list=JSON.parse(data)
+}
+app.set('view engine','ejs')
+app.get('/',(req,res)=>{
+    res.render('index',{list,showData:null})
+})
+app.get('/showData',(req,res)=>{
+    const eId=req.query.show
+    let showData=list[eId]
+    res.render('product',{showData,list})
+})
+app.get('/return',(req,res)=>{
+    res.render('index')
+})
+app.listen(2104)
