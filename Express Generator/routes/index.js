91abeb8d6971e04dad4997838b4ff1febe2154c3
AMIT <156776217+Amitkalsariya@users.noreply.github.com>
2024-11-27 17:50:53 +0530

    D

--- Express Generator/routes/index.js
+++ Express Generator/routes/index.js
@@ -2,12 +2,15 @@ var express = require('express');
 var router = express.Router();
 const   USER=require('../Models/user')
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async(req, res, next)=> {
+  const data=await USER.find(req.body)
+  res.render('index', { title: 'Express',data });
 });
- router.post ('/',async (req,res)=>{
+ router.post ('/add',async (req,res)=>{
   console.log(req.body);
   await USER.create(req.body)
   res.redirect('/')
  })
+ 
 module.exports = router;
+ 
